052754557d0323ccfbc75675298717843be43219
derpzort <44878066+derpzort@users.noreply.github.com>
2020-01-05 02:50:43 -0800

    Add files via upload

---
+++ assets/vendor5.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+  $('.iRow').click(function () {
+
+    let show = $(this).attr("show");
+
+    if (show == "1") {
+      $(this).children().children("i").toggle();
+
+      $(this).next("tr")
+        .children('td, th')
+        .animate({ padding: 0 })
+        //.wrapInner('<div />')
+        .children()
+        .slideUp(function () { $(this).closest('tr').toggle(); });
+
+      $(this).attr("show", 0);
+
+    } else {
+
+      $(this).children().children("i").toggle();
+
+      $(this).next("tr")
+        .children('td, th')
+        .animate({ padding: 5 })
+        // .wrapInner('<div />')
+        .children()
+        .slideDown(function () { $(this).closest('tr').toggle(); });
+
+      $(this).attr("show", 1);
+
+    }
+
+
+  });
+
+  $('.aRow').click(function () {
+
+    let show = $(this).attr("show");
+
+    if (show == "1") {
+
+      $(this).children().children("i").toggle();
+
+      $(this).next("tr")
+        .children('td, th')
+        .animate({ padding: 0 })
+        //.wrapInner('<div />')
+        .children()
+        .slideUp(function () { $(this).closest('tr').toggle(); });
+
+      $(this).attr("show", 0);
+
+    } else {
+
+      $(this).children().children("i").toggle();
+
+      $(this).next("tr")
+        .children('td, th')
+        .animate({ padding: 5 })
+        // .wrapInner('<div />')
+        .children()
+        .slideDown(function () { $(this).closest('tr').toggle(); });
+
+      $(this).attr("show", 1);
+
+    }
+
+
+  });
+
+  $("#linkA").click(function () {
+    //$("#linkAsec").toggle(500);
+    $("#linkAsec").slideToggle("slow");
+  });
+
+  $("#linkB").click(function () {
+    $("#linkBsec").slideToggle(500);
+  });
+
+});
